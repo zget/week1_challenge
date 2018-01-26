8022b2e65f160601d7f2a2163acc49e04702227c
zget <dgetahun84@gmail.com>
2018-01-25 19:09:40 -0500

    formated

--- src/main/java/fizzbuzzapp/demo/fizzBuzzclass.java
+++ src/main/java/fizzbuzzapp/demo/fizzBuzzclass.java
@@ -11,13 +11,13 @@ public class fizzBuzzclass
     {
         String message_from_method=" ";
         if(i%3==0 && i%5==0)
-            message_from_method="FizzBuzz";
+            message_from_method="FizzBuzz<br/>";
         else if(i%3==0)
-            message_from_method="Fizz";
+            message_from_method="Fizz<br/>";
         else if(i%5==0)
-            message_from_method="Buzz";
+            message_from_method="Buzz<br/>";
         else
-            message_from_method=Integer.toString(i);
+            message_from_method=Integer.toString(i)+"<br/>";
 
         return message_from_method;
     }
